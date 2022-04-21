c87d1d389625f5918ad08b52b2d1be0b53375332
abbas <abbas@infofactors.com>
2022-04-21 16:07:04 +0530

    day 1

---
+++ Day-1/hello-world/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class App extends Component{
+  render() {
+    return (
+      <div>
+        <h1>Hello world</h1>
+        <h3>This is my first react application</h3>
+      </div>
+    )
+  }
+}
+
+export default App
+
